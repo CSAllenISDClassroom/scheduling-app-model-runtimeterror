430b70cfc379435dea9fd7c8586d4e260f985d7b
William Jackson <william.jackson@student.allenisd.org>
2022-02-01 15:23:16 -0600

    Fixed subcategories (there were no errors earlier, however it was not functional)

--- VaporApp/Sources/VaporShell/App/Controllers/SubcategoriesController.swift
+++ VaporApp/Sources/VaporShell/App/Controllers/SubcategoriesController.swift
@@ -4,8 +4,8 @@ import FluentMySQLDriver
 
 public class SubcategoryController {
 
-    public func getCategories(_app: Application) throws {
-        app.get("Subcategories") { req -> Page<Subcategory> in
+    public func getCategories(_ app: Application) throws {
+        app.get("subcategories") { req -> Page<Subcategory> in
             let subcategoriesData = try await SubcategoryData.query(on: req.db)
               .paginate(for: req)
             let subcategories = try subcategoriesData.map { try Subcategory(data: $0) }
